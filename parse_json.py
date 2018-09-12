9c73bfa29d9932df8e38b08ffba25924b0ed855b
mortalcatalyst <flebber.crue@gmail.com>
2018-09-12 19:09:41 +1000

    Added system detection

--- parse_json.py
+++ parse_json.py
@@ -1,27 +1,41 @@
 # -*- coding: utf-8
 import json
+import platform
+
+print(platform.system())
 #
 # wanted_keys = ["RaceNumber", "RaceTime", "RaceName", "Distance", "WeatherCondition", "WeatherConditionLevel",
 #                "TrackCondition", "TrackConditionLevel", "TrackRating"]
 #
 # deeper_keys = ["Runners", "Results"]
 #
-with open(r'C:\Users\Sayth\OneDrive\Projects\Folder\Results\Rosehill_2018-06-30.json', 'r') as f:
+
+
+def system_folder():
+    if platform.system() == 'Linux':
+        linux_folder = r'/home/sayth/OneDrive/Projects/Folder/Results/Rosehill_2018-06-30.json'
+        return linux_folder
+    else:
+        windows_folder = r'C:\Users\Sayth\OneDrive\Projects\Folder\Results\Rosehill_2018-06-30.json'
+        return windows_folder
+
+
+with open(system_folder(), 'r') as f:
     DATA = json.load(f)
     ENTRY_POINT = DATA['RaceDay']['Meetings']
     for item in ENTRY_POINT:
         for dets in item['Races']:
-            for run in  dets['RacingFormGuide']['Meeting']:
-                print(run.items())
+            for run in dets['RacingFormGuide']['Meeting']:
+                print(run)
 #             # for det in dets.items():  #     print(det)  #     print(det['RunnerName'])  #     if k in deeper_keys:  #  #       print(v)  #         print("{0} \t {1}".format(k, v))
 #
 #
-print(ENTRY_POINT.keys())
+    print(ENTRY_POINT.keys())
 #  dict_keys(['Abandoned', 'MeetingId', 'MeetingCode', 'MeetingType', 'VenueName', 'WeatherCondition',  # 'WeatherConditionLevel', 'TrackCondition', 'TrackConditionLevel', 'TrackRating', 'TrackChanged',  #  'TrackRatingChanged', 'Races', 'Pools'])
 #
 # ## Meeting details level.  # print("MeetingId: ", ENTRY_POINT['MeetingId'])  # print("Venue Name: ", ENTRY_POINT['VenueName'])  # print("Weather: ", ENTRY_POINT['WeatherCondition'])  # print("Track Condition: ", ENTRY_POINT['TrackCondition'])  # print("Track Rating: ", ENTRY_POINT['TrackRating'])
 #
-races = ENTRY_POINT['Races'][0]
+    races = ENTRY_POINT['Races'][0]
 #
 # ## First level of feature keys  # dict_keys(['FeatureRaceBonusActive', 'FixedPriceSummary', 'RacingFormGuide', 'Status', 'RaceNumber',  # 'RaceTime', 'RaceName', 'Distance', 'WeatherChanged', 'WeatherCondition', 'WeatherConditionLevel',  # 'TrackChanged', 'TrackCondition', 'TrackConditionLevel', 'TrackRating', 'SubFavourite',  # 'SubFavouriteCandidate', 'TotalTrioAvailable', 'TotalTrioSubEventId', 'TrackRatingChanged', 'Runners',  #  'Pools', 'Tips', 'Results'])
 #
@@ -31,11 +45,11 @@ races = ENTRY_POINT['Races'][0]
 #
 # # print(list(RFG))  # print(list(EVENT))
 
-def walk_json(tree, path=[]):
-    try:
-        for root in tree.keys():
-            yield from walk_json(path + [root])
-    except AttributeError:  # in case .items() is not possible (on leaves)
-        yield path + [tree]
+    def walk_json(tree, path=[]):
+        try:
+            for root in tree.keys():
+                yield from walk_json(path + [root])
+        except AttributeError:  # in case .items() is not possible (on leaves)
+            yield path + [tree]
 #
 # # print(list(walk_json(races['RacingFormGuide']['Event']['Race']['Distance'])))
